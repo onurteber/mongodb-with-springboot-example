658e5984767aac79043d868a076b10528cb315cb
Onur Teber <onur.teber@softtech.com.tr>
2020-10-28 15:09:36 +0300

    added crud operations

--- spring-mongodb/src/main/java/com/spring/mongodb/springmongodb/resource/HotelController.java
+++ spring-mongodb/src/main/java/com/spring/mongodb/springmongodb/resource/HotelController.java
@@ -1,10 +1,14 @@
 package com.spring.mongodb.springmongodb.resource;
 
+import com.spring.mongodb.springmongodb.model.Hotel;
 import com.spring.mongodb.springmongodb.repository.HotelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/hotels")
@@ -14,7 +18,33 @@ public class HotelController {
     private HotelRepository repository;
 
     @GetMapping("/all")
-    public ResponseEntity<?> getAll() {
-        return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
+    public List<Hotel> getAll() {
+        return repository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable("id") String id) {
+        if (repository.existsById(id))
+            return new ResponseEntity<>(repository.findById(id), HttpStatus.OK);
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Hotel hotel) {
+        repository.save(hotel);
+        return new ResponseEntity<>(hotel, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody Hotel hotel) {
+        repository.save(hotel);
+        return new ResponseEntity<>(hotel, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") String id) {
+        repository.deleteById(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
 }
